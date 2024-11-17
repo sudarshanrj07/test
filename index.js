105cf7b3f90c22e4e20009f58f5504f2451564e9
Sudarshan Jalawadi <sudarshanjalawadi@Mjolnir.local>
2024-11-17 23:08:46 +0530

    Changed the greeting message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
 	res.writeHead(200, { "content-type": "text/plain" });
-	res.end("Hell World");
+	res.end("Welcome To My First Deployment");
 });
 
 const PORT = process.env.PORT || 3000;
