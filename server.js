7bce80f331b70adae6233f88ff11357a986aa928
sudarshanrj07 <srjalawadi07@gmail.com>
2024-12-05 14:00:51 +0530

    added required modules

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 3000;
 
 // Serve static files from the "public" directory
